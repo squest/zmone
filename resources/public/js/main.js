e3d31dc759fafdae266ab97fd6e4f1f37238308c
squest <sabdaps@gmail.com>
2019-02-18 23:33:40 +0700

    changing the AppBar

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -6,12 +6,37 @@ import ReactDOM from 'react-dom';
 import Button from '@material-ui';
 */
 
-function App() {
+function App(props) {
+    const styles = {
+        root: {
+            flexGrow: 1,
+        },
+        grow: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginLeft: -12,
+            marginRight: 20,
+        },
+    };
+
+    const {classes} = props;
+
     return (
-        <Button variant = "contained" color = "primary">
-        Hello World
-        </Button>
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                    </IconButton>
+                    <Typography variant="h6" color="inherit">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     );
 }
 
-ReactDOM.render( <App/>, document.querySelector('#app'));
+
+ReactDOM.render( <App classes={withStyles}/>, document.querySelector('#app'));
