b8e22abd12f9bc6746de8afb611ced49e2a55003
squest <sabdaps@gmail.com>
2019-02-19 12:28:27 +0700

    more json-body

--- src/zmone/web/routes.clj
+++ src/zmone/web/routes.clj
@@ -15,12 +15,29 @@
                   response                                  :response
                   datomic                                   :datomic
                   :as                                       context}]
+              (println form-params)
+              (println session)
               (->> (selmer/render-file "public/html/index.html" {})
+                   (ring-resp/response)
+                   (assoc context :response)))}))
+(def api-page
+  "dummy interceptor"
+  (interceptor
+    {:name  ::api-page
+     :enter (fn [{{:keys [session form-params] :as request} :request ;; get path-params or body-params or query-params or json-params here
+                  response                                  :response
+                  datomic                                   :datomic
+                  :as                                       context}]
+              (println form-params)
+              (println session)
+              (->> {:sabda  "this is it"
+                    :gerald [12 321 1234 32]}
                    (ring-resp/response)
                    (assoc context :response)))}))
 
 (def routes
   "initroute"
-  [["/" :get [home-page] :route-name :home-page]])
+  [["/" :get [home-page] :route-name :home-page]
+   ["/jojon" :get [http/json-body api-page] :route-name :api-page]])
 
 (defn make-routes [config] routes)
